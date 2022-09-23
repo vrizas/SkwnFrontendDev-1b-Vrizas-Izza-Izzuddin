929e1b744d3476411854084ec0633eead2f478d8
vrizas <vrizasizza@gmail.com>
2022-09-24 03:12:06 +0700

    create finance page

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,15 @@
-import profilePic from '../assets/images/profile-pic.jpg';
-import Service from './Service';
-import Visa from './Visa';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import FinancePage from '../pages/FinancePage';
+import HomePage from '../pages/HomePage';
 
 function App() {
   return (
     <div className="max-w-[600px] min-h-screen bg-gray-100 m-auto">
-      <div className="px-4 py-3">
-        <header className="flex justify-between">
-          <div>
-            <p className="text-gray-600">Hello,</p>
-            <h1 className="font-bold text-xl">Aurellia!</h1>
-          </div>
-          <img src={profilePic} alt="" className="w-14 h-14 rounded-full" />
-        </header>
-        <main>
-          <Visa />
-          <Service />
-        </main>  
-      </div>
-      
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/finance" element={<FinancePage />} />
+      </Routes>  
     </div>
   );
 }
--- src/components/Service.js
+++ src/components/Service.js
@@ -1,5 +1,6 @@
 /* eslint-disable */
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { BsCreditCardFill, BsQuestionLg } from 'react-icons/bs';
 import { FaDollarSign } from 'react-icons/fa';
 import { BiStats } from 'react-icons/bi';
@@ -12,18 +13,18 @@ function Service() {
                 <a href="#" className="text-gray-600 text-sm">See All</a>
             </div>
             <div className="mt-4 flex justify-between font-medium text-gray-600">
-                <div className="flex flex-col items-center gap-1 text-sm">
-                    <span className="w-10 h-10 rounded-full bg-gray-200 text-sky-500 flex justify-center items-center"><BsCreditCardFill size="20" /></span> Card
-                </div>
-                <div className="flex flex-col items-center gap-1 text-sm">
-                    <span className="w-10 h-10 rounded-full bg-gray-200 text-sky-500 flex justify-center items-center"><FaDollarSign size="20" /></span> Finance
-                </div>
-                <div className="flex flex-col items-center gap-1 text-sm">
-                    <span className="w-10 h-10 rounded-full bg-gray-200 text-sky-500 flex justify-center items-center"><BiStats size="20" /></span> Top Up
-                </div>
-                <div className="flex flex-col items-center gap-1 text-sm">
-                    <span className="w-10 h-10 rounded-full bg-gray-200 text-sky-500 flex justify-center items-center"><BsQuestionLg size="20" /></span> Help
-                </div>
+                <Link to="/finance" className="flex flex-col items-center gap-1 text-sm">
+                    <span className="w-10 h-10 rounded-full bg-blue-100 text-sky-500 flex justify-center items-center"><BsCreditCardFill size="20" /></span> Card
+                </Link>
+                <Link to="/finance" className="flex flex-col items-center gap-1 text-sm">
+                    <span className="w-10 h-10 rounded-full bg-blue-100 text-sky-500 flex justify-center items-center"><FaDollarSign size="20" /></span> Finance
+                </Link>
+                <Link to="/finance" className="flex flex-col items-center gap-1 text-sm">
+                    <span className="w-10 h-10 rounded-full bg-blue-100 text-sky-500 flex justify-center items-center"><BiStats size="20" /></span> Top Up
+                </Link>
+                <Link to="/finance" className="flex flex-col items-center gap-1 text-sm">
+                    <span className="w-10 h-10 rounded-full bg-blue-100 text-sky-500 flex justify-center items-center"><BsQuestionLg size="20" /></span> Help
+                </Link>
             </div>
         </div>
     )
